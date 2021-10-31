23ef3534def2655fd3cfc1f98f35191d76ee3f7b
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-30 21:07:19 -0400

    redirect animation

--- docs/tournament/src/countdown.js
+++ docs/tournament/src/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = startDate.getTime();
+var countDownDate = new Date("Nov 19 2021, 0:0:0 UTC").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,12 +17,18 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the countdown elements
-  document.getElementById("days").innerHTML = days
-  document.getElementById("hours").innerHTML = hours
-  document.getElementById("minutes").innerHTML = minutes
-  document.getElementById("seconds").innerHTML = seconds
+  if (distance >= 0) {
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
+  }
   // If the count down is finished, write some text
   if (distance < 0) {
+    document.getElementById("days").innerHTML = 0
+    document.getElementById("hours").innerHTML = 0
+    document.getElementById("minutes").innerHTML = 0
+    document.getElementById("seconds").innerHTML = 0
     clearInterval(x);
     fc37ce8540d5b05a94c69f1d145d();
   }
--- docs/tournament/src/index.js
+++ docs/tournament/src/index.js
@@ -1,6 +1,29 @@
 async function fc37ce8540d5b05a94c69f1d145d() {
-  await sleep(1000)
-  window.location.href = ""
+  let textContainer = document.getElementsByClassName("text-container")[0]
+  let body = document.getElementsByTagName("body")[0]
+  let html = document.getElementsByTagName("html")[0]
+  let background = document.getElementsByClassName("sliding-background")[0]
+  let slide = document.getElementsByClassName("slide")[0]
+  let img = document.getElementsByClassName("img")[0]
+
+  body.style.transition = "1s ease-in-out"
+  body.style.background = "#fcf5e8"
+  body.style.overflow = "hidden"
+  textContainer.style.animation = "shake 0.2s linear infinite"
+  textContainer.style.filter = "invert(1)";
+  background.style.animation = "slide 0.5s linear infinite"
+  background.style.filter = "blur(0) drop-shadow(0 0 1px black)"
+  await sleep(2000)
+  img.style.animation = "unblur 2s ease-in-out"
+  textContainer.style.filter = "invert(0)";
+  background.style.animation = "slide 0.2s linear infinite"
+  slide.style.animation = "move-up 7s ease-in-out infinite"
+  await sleep(2000)
+  img.style.filter = "opacity(1) blur(0)"
+  background.style.animation = "slide 0.1s linear infinite"
+  textContainer.style.animation = "shake 0.1s linear infinite"
+  await sleep(3000)
+  window.location.href = "https://cdc.clxxiii.dev"
 }
 
 async function sleep(ms) {
