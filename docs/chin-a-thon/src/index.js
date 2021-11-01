27f461e00d29a837f4e1c209b9de01207ce3881b
clxxiii <earthnoartisjusteh@gmail.com>
2021-11-01 19:40:39 -0400

    more formatting

--- docs/chin-a-thon/src/index.js
+++ docs/chin-a-thon/src/index.js
@@ -1,8 +1,13 @@
-async function onLoad() {
-    await sleep(5000);
-    window.location.reload();
-}
+/*
+ * this is literally just a function so i don't have to hit 
+ * Ctrl + R while working on the webpage
+ */
 
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
+// async function onLoad() {
+//     await sleep(5000);
+//     window.location.reload();
+// }
+
+// async function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms))
+// }
